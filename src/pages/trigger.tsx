b6da4da41adda6e7e83b96f78bfaca442c47f384
Illenata <khoverko@gmail.com>
2022-03-29 14:07:12 +0300

    remove comments

--- src/pages/trigger.tsx
+++ src/pages/trigger.tsx
@@ -105,10 +105,6 @@ const AnimationOnTrigger = (): JSX.Element => {
 
   useIntersectionObserver(options, '0px', 1, setCountAnimation, () => null, isUnobserveble)
 
-  // useIntersectionObserver(firstTextRef, '0px', 1, () => setCountAnimation(0), () => null, isUnobserveble)
-  // useIntersectionObserver(secondTextRef, '0px', 1, () => setCountAnimation(1), () => null, isUnobserveble)
-  // useIntersectionObserver(thirdTextRef, '0px', 1, () => setCountAnimation(2), () => null, isUnobserveble)
-
   return (
     <PageComponent
       title="Animation on trigger"
