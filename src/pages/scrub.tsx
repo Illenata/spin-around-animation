cc354cfd665930fa6021b48f384bfab531022b12
Illenata <khoverko@gmail.com>
2022-04-04 14:36:43 +0300

    remove unnecessary if block

--- src/pages/scrub.tsx
+++ src/pages/scrub.tsx
@@ -27,44 +27,42 @@ const Scrub = (): JSX.Element => {
         animationTime = duration
       }
 
-      if (animationTime >= 0 && animationTime <= duration) {
-        const getTransformProps = (deg: string, rotate: string, scale: string) => ({
-          transform: `rotate(${deg}deg) translateX(200px) rotate(${rotate}deg) scale(${scale})`
-        })
+      const getTransformProps = (deg: string, rotate: string, scale: string) => ({
+        transform: `rotate(${deg}deg) translateX(200px) rotate(${rotate}deg) scale(${scale})`
+      })
 
-        const animationItems = [{
-          ref: shopersIconRef.current,
-          transformOptions: [
-            getTransformProps('-120', '120', '1'),
-            getTransformProps('0', '0', '1.354'),
-            getTransformProps('120', '-120', '1'),
-          ],
-        },
-        {
-          ref: independentIconRef.current,
-          transformOptions: [
-            getTransformProps('0', '0', '1.354'),
-            getTransformProps('120', '-120', '1'),
-            getTransformProps('240', '-240', '1'),
-          ],
-        },
-        {
-          ref: employersIconRef.current,
-          transformOptions: [
-            getTransformProps('120', '-120', '1'),
-            getTransformProps('240', '-240', '1'),
-            getTransformProps('360', '-360', '1.354'),
-          ],
-        }]
+      const animationItems = [{
+        ref: shopersIconRef.current,
+        transformOptions: [
+          getTransformProps('-120', '120', '1'),
+          getTransformProps('0', '0', '1.354'),
+          getTransformProps('120', '-120', '1'),
+        ],
+      },
+      {
+        ref: independentIconRef.current,
+        transformOptions: [
+          getTransformProps('0', '0', '1.354'),
+          getTransformProps('120', '-120', '1'),
+          getTransformProps('240', '-240', '1'),
+        ],
+      },
+      {
+        ref: employersIconRef.current,
+        transformOptions: [
+          getTransformProps('120', '-120', '1'),
+          getTransformProps('240', '-240', '1'),
+          getTransformProps('360', '-360', '1.354'),
+        ],
+      }]
 
-        animationItems.forEach(({ ref, transformOptions }) => {
-          const options = { duration, fill: 'both'} as KeyframeAnimationOptions
-          
-          const animation = ref?.animate(transformOptions, options)
-          animation?.pause()
-          animation!.currentTime = animationTime
-        })
-      }
+      animationItems.forEach(({ ref, transformOptions }) => {
+        const options = { duration, fill: 'both'} as KeyframeAnimationOptions
+        
+        const animation = ref?.animate(transformOptions, options)
+        animation?.pause()
+        animation!.currentTime = animationTime
+      })
     }
   }
 
